fe74547b342a7f7b129411c6753f6979bf1ae61b
pcabra96 <pablocabram@gmail.com>
2023-06-16 14:46:12 +0200

    FARIMA

--- Reproducibility/Main/FARIMA/FARIMA (1,d,1) simulation.R
+++ Reproducibility/Main/FARIMA/FARIMA (1,d,1) simulation.R
@@ -17,10 +17,10 @@ set.seed(0)
 
 PROCESS = "FARIMA"
 POWER = 7:14
-N_SIMULATIONS = 1000
+N_SIMULATIONS = 100
 ar_coef_vec = runif(N_SIMULATIONS, min = -0.9, max = 0.9)
 ma_coef_vec = runif(N_SIMULATIONS, min = -0.9, max = 0.9)
-d_coef = runif(N_SIMULATIONS, min = 0.01, max = 0.49)
+d_coef_vec = runif(N_SIMULATIONS, min = 0, max = 0.5)
 
 names=c(TeX("$2^7$"), TeX("$2^8$"), TeX("$2^9$"), TeX("$2^{10}$"), TeX("$2^{11}$"), TeX("$2^{12}$"), TeX("$2^{13}$"),TeX("$2^{14}$"))
 
@@ -42,6 +42,10 @@ fit_own_theta = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
 fit_r_phi = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
 fit_r_theta = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
 
+# d coefficient
+fit_r_d = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+fit_own_d = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+
 # LAMBDA
 fit_own_exp = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
 fit_r_exp = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
@@ -54,26 +58,30 @@ p_val_r_exp = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
 # SIMULATION
 ################################################################################
 
+start = Sys.time()
 for (sim in 1:N_SIMULATIONS) {
   for (j in 1:length(POWER)) {
     T = 2^(POWER[j])
     ar_coef = ar_coef_vec[sim]
     ma_coef = ma_coef_vec[sim]
+    d_coef = d_coef_vec[sim]
+
     if(0.05268817>abs(ma_coef+ar_coef)){
       ma_coef_vec[sim] = -ma_coef
       ma_coef = -ma_coef
     }
-    true_spectrum = farima.spectrum(ar = ar_coef,ma= ma_coef, n.freq = T)
+
+    true_spectrum = farima.spectrum(ar = ar_coef,ma= ma_coef, d = d_coef, n.freq = T)
 
     # ARMA(1,1) OWN simulations
     start_own = Sys.time()
-    y_own = sim.farima(ar = ar_coef, ma = ma_coef, T = T)
+    y_own = sim.farima(ar = ar_coef, ma = ma_coef, d = d_coef, T = T)
     end_own = Sys.time()
     time_own[sim,j] = end_own-start_own
 
     # AR EXISTING PACKAGE
     start_r = Sys.time()
-    y_r = arima.sim(model = list(ar = ar_coef, ma = ma_coef), n = T)
+    y_r = fracdiff.sim(n = T, ar = ar_coef, ma = ma_coef, d = d_coef)[["series"]]
     end_r = Sys.time()
     time_r[sim,j] = end_r-start_r
 
@@ -81,13 +89,15 @@ for (sim in 1:N_SIMULATIONS) {
     # FITTING
     ################################################################################
 
-    a = fit.farima(y_own, p = 1,q = 1)
+    a = fit.farima(y_own, p = 1,q = 1, d = 1)
     fit_own_phi[sim,j] = a[["ar"]]
     fit_own_theta[sim,j] = a[["ma"]]
+    fit_own_d[sim,j] = a[["d"]]
 
-    a = fit.farima(y_r, p = 1, q = 1)
+    a = fit.farima(y_r, p = 1, q = 1, d = 1)
     fit_r_phi[sim,j] = a[["ar"]]
     fit_r_theta[sim,j] = a[["ma"]]
+    fit_r_d[sim,j] = a[["d"]]
 
     ################################################################################
     # PERIODOGRAM
@@ -118,13 +128,16 @@ for (sim in 1:N_SIMULATIONS) {
     p_val_r_exp[sim,j] = ks.test(I_r, "pexp", 1)[["p.value"]]
   }
 }
-
+end = Sys.time()
+total_time = end-start
+print(total_time)
 ################################################################################
 # SAVE AR and MA parameteres
 ################################################################################
 
 saveRDS(ar_coef_vec, file = paste0(path,"ar_coef_vec.RData"))
 saveRDS(ma_coef_vec, file = paste0(path,"ma_coef_vec.RData"))
+saveRDS(d_coef_vec, file = paste0(path,"d_coef_vec.RData"))
 
 ################################################################################
 # Running time
@@ -155,6 +168,16 @@ colnames(fit_r_theta) = POWER
 
 saveRDS(fit_own_theta, file = paste0(path,"theta_1_own.RData"))
 saveRDS(fit_r_theta, file = paste0(path,"theta_1_r.RData"))
+
+################################################################################
+# MSE COEFFICIENTS for d
+################################################################################
+
+colnames(fit_own_d) = POWER
+colnames(fit_r_d) = POWER
+
+saveRDS(fit_own_d, file = paste0(path,"d_own.RData"))
+saveRDS(fit_r_d, file = paste0(path,"d_r.RData"))
 
 ################################################################################
 # MSE COEFFICIENTS for lambda=1

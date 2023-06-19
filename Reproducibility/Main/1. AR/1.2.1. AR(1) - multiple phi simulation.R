54ba56643fb3ad388dc60754545a5fc4058a73a6
pcabra96 <pablocabram@gmail.com>
2023-06-19 15:04:14 +0200

    jkl

---
+++ Reproducibility/Main/1. AR/1.2.1. AR(1) - multiple phi simulation.R
@@ -0,0 +1,219 @@
+################################################################################
+##----------------------------------------------------------------------------##
+## INDEX                                                                      ##
+##----------------------------------------------------------------------------##
+################################################################################
+
+# 1. PACKAGES
+# 2. SEED
+# 3. SIMULATION PARAMETERS
+# 4. SIMULATION
+# 5. RESULTS
+# 5.1. TIME
+# 5.2. TIME DOMAIN PARAMETER
+# 5.3. FREQUENCY DOMAIN PARAMETER
+# 5.4. FREQUENCY DOMAIN GOODNESS OF FIT
+
+################################################################################
+##----------------------------------------------------------------------------##
+## 1. PACKAGES                                                                ##
+##----------------------------------------------------------------------------##
+################################################################################
+
+library(forecast)
+require(MASS)
+
+################################################################################
+##----------------------------------------------------------------------------##
+## 2. SEED                                                                    ##
+##----------------------------------------------------------------------------##
+################################################################################
+
+set.seed(0)
+
+################################################################################
+##----------------------------------------------------------------------------##
+## 3. SIMULATION PARAMETERS                                                   ##
+##----------------------------------------------------------------------------##
+################################################################################
+
+PROCESS = "AR"
+ar_coef_vec = c(-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9)
+POWER = 7:14
+N_SIMULATIONS = 1000
+names=c(TeX("$2^7$"), TeX("$2^8$"), TeX("$2^9$"), TeX("$2^{10}$"), TeX("$2^{11}$"), TeX("$2^{12}$"), TeX("$2^{13}$"),TeX("$2^{14}$"))
+
+################################################################################
+##----------------------------------------------------------------------------##
+## 4. SIMULATION                                                              ##
+##----------------------------------------------------------------------------##
+################################################################################
+
+fit_own_coef_list = replicate(10, matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER)), simplify = FALSE)
+fit_r_coef_list = replicate(10, matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER)), simplify = FALSE)
+fit_own_exp_list = replicate(10, matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER)), simplify = FALSE)
+fit_r_exp_list = replicate(10, matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER)), simplify = FALSE)
+p_val_own_exp_list = replicate(10, matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER)), simplify = FALSE)
+p_val_r_exp_list = replicate(10, matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER)), simplify = FALSE)
+time_own_list = replicate(10, matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER)), simplify = FALSE)
+time_r_list = replicate(10, matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER)), simplify = FALSE)
+
+for (param_number in 1:length(ar_coef_vec)) {
+  ar_coef = ar_coef_vec[param_number]
+  fit_own_coef = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+  fit_r_coef = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+  fit_own_exp = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+  fit_r_exp = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+  p_val_own_exp = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+  p_val_r_exp = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+  time_own = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+  time_r = matrix(0,nrow = N_SIMULATIONS, ncol = length(POWER))
+
+  for (j in 1:length(POWER)) {
+    T = 2^(POWER[j])
+    true_spectrum = farima.spectrum(ar = ar_coef, n.freq = T)
+    for (sim in 1:N_SIMULATIONS) {
+
+      # AR OWN simulations
+      start_own = Sys.time()
+      y_own = sim.farima(ar = ar_coef, T = T)
+      end_own = Sys.time()
+      time_own[sim,j] = end_own-start_own
+      # AR EXISTING PACKAGE
+      start_r = Sys.time()
+      y_r = arima.sim(model = list(ar = ar_coef), n = T)
+      end_r = Sys.time()
+      time_r[sim,j] = end_r-start_r
+
+      ################################################################################
+      # FITTING
+      ################################################################################
+
+      fit_own_coef[sim,j] = fit.farima(y_own, p = 1)[["ar"]]
+      fit_r_coef[sim,j] = fit.farima(y_r, p = 1)[["ar"]]
+
+      ################################################################################
+      # PERIODOGRAM
+      ################################################################################
+
+      n = length(y_own)
+
+      # Fundamental frequencies
+      mhalfm <- (n-1) %/% 2L
+      w <- 2*pi/n * (1:mhalfm)
+
+      # Periodogram ordinates by FFT of own simulation
+      per_own = (Mod(fft(y_own))^2/(2*pi*n)) [1:(n %/% 2 + 1)]
+      yper_own = per_own[2: ((n+1) %/% 2)]
+      I_own = yper_own/true_spectrum[1:(T/2-1)]
+
+      # Periodogram ordinates by FFT of R simulation
+      per_r = (Mod(fft(y_r))^2/(2*pi*n)) [1:(n %/% 2 + 1)]
+      yper_r = per_r[2: ((n+1) %/% 2)]
+      I_r = yper_r/true_spectrum[1:(T/2-1)]
+
+      # Periodogram distribution of own simulation
+      fit_own_exp[sim,j] <- fitdistr(I_own, "exponential")[["estimate"]]
+      p_val_own_exp[sim,j] = ks.test(I_own, "pexp", 1)[["p.value"]]
+
+      # Periodogram distribution of R simulation
+      fit_r_exp[sim,j] <- fitdistr(I_r, "exponential")[["estimate"]]
+      p_val_r_exp[sim,j] = ks.test(I_r, "pexp", 1)[["p.value"]]
+    }
+  }
+  fit_own_coef_list[[param_number]] = fit_own_coef
+  fit_r_coef_list[[param_number]] = fit_r_coef
+  fit_own_exp_list[[param_number]] = fit_own_exp
+  fit_r_exp_list[[param_number]] = fit_r_exp
+  p_val_own_exp_list[[param_number]] = p_val_own_exp
+  p_val_r_exp_list[[param_number]] = p_val_r_exp
+  time_own_list[[param_number]] = time_own
+  time_r_list[[param_number]] = time_r
+}
+
+################################################################################
+##----------------------------------------------------------------------------##
+## 5. RESULTS                                                                 ##
+##----------------------------------------------------------------------------##
+################################################################################
+
+path = paste0("~/Documents/2. UNIGE/2023-1 Master Thesis/fexpsmt/Reproducibility/Main/1. ",PROCESS,"/")
+
+################################################################################
+# 5.1. TIME
+################################################################################
+
+average_time_own = matrix(0, nrow = length(ar_coef_vec), ncol = length(POWER))
+average_time_r = matrix(0, nrow = length(ar_coef_vec), ncol = length(POWER))
+for (i in 1:length(ar_coef_vec)) {
+  average_time_own[i,] = colMeans((time_own_list[[i]]))
+  average_time_r[i,] = colMeans((time_r_list[[i]]))
+}
+
+# Visualization
+rownames(average_time_own) = ar_coef_vec
+colnames(average_time_own) = POWER
+rownames(average_time_r) = ar_coef_vec
+colnames(average_time_r) = POWER
+
+saveRDS(average_time_own, file = paste0(path,"time_own.RData"))
+saveRDS(average_time_r, file = paste0(path,"time_R.RData"))
+
+################################################################################
+# 5.2. TIME DOMAIN PARAMETER
+################################################################################
+
+MSE_own = matrix(0, nrow = length(ar_coef_vec), ncol = length(POWER))
+MSE_r = matrix(0, nrow = length(ar_coef_vec), ncol = length(POWER))
+for (i in 1:length(ar_coef_vec)) {
+  MSE_own[i,] = colMeans((fit_own_coef_list[[i]]-ar_coef_vec[i])^2)
+  MSE_r[i,] = colMeans((fit_r_coef_list[[i]]-ar_coef_vec[i])^2)
+}
+
+rownames(MSE_own) = ar_coef_vec
+colnames(MSE_own) = POWER
+rownames(MSE_r) = ar_coef_vec
+colnames(MSE_r) = POWER
+
+saveRDS(MSE_own, file = paste0(path,"phi_1_own.RData"))
+saveRDS(MSE_r, file = paste0(path,"phi_1_r.RData"))
+
+################################################################################
+# 5.3. FREQUENCY DOMAIN PARAMETER
+################################################################################
+
+# OWN CODE
+MSE_own = matrix(0, nrow = length(ar_coef_vec), ncol = length(POWER))
+MSE_r = matrix(0, nrow = length(ar_coef_vec), ncol = length(POWER))
+for (i in 1:length(ar_coef_vec)) {
+  MSE_own[i,] = colMeans((fit_own_exp_list[[i]]-1)^2)
+  MSE_r[i,] = colMeans((fit_r_exp_list[[i]]-1)^2)
+}
+
+rownames(MSE_own) = ar_coef_vec
+colnames(MSE_own) = POWER
+rownames(MSE_r) = ar_coef_vec
+colnames(MSE_r) = POWER
+
+saveRDS(MSE_own, file = paste0(path,"lambda_own.RData"))
+saveRDS(MSE_r, file = paste0(path,"lambda_r.RData"))
+
+################################################################################
+# 5.4. FREQUENCY DOMAIN GOODNESS OF FIT
+################################################################################
+
+# OWN CODE
+prop_non_rejection_own = matrix(0, nrow = length(ar_coef_vec), ncol = length(POWER))
+prop_non_rejection_r = matrix(0, nrow = length(ar_coef_vec), ncol = length(POWER))
+for (i in 1:length(ar_coef_vec)) {
+  prop_non_rejection_own[i,] = colMeans(p_val_own_exp_list[[i]]>=0.05)
+  prop_non_rejection_r[i,] = colMeans(p_val_r_exp_list[[i]]>=0.05)
+}
+
+rownames(prop_non_rejection_own) = ar_coef_vec
+colnames(prop_non_rejection_own) = POWER
+rownames(prop_non_rejection_r) = ar_coef_vec
+colnames(prop_non_rejection_r) = POWER
+
+saveRDS(prop_non_rejection_own, file = paste0(path,"p.val_own.RData"))
+saveRDS(prop_non_rejection_r, file = paste0(path,"p.val_R.RData"))
---
+++ Reproducibility/Main/2. MA/2.2.2. MA(1) - multiple theta visualization.R
@@ -0,0 +1,114 @@
+################################################################################
+# PACKAGES
+################################################################################
+
+library(forecast)
+require(MASS)
+library(latex2exp)
+library(xtable)
+library(Hmisc)
+library(kableExtra)
+library(plot.matrix)
+
+################################################################################
+# PARAMETERS
+################################################################################
+
+ma_coef_vec = c(-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9)
+POWER = 7:14
+N_SIMULATIONS = 1000
+names = c(TeX("$2^7$"), TeX("$2^8$"), TeX("$2^9$"), TeX("$2^{10}$"), TeX("$2^{11}$"), TeX("$2^{12}$"), TeX("$2^{13}$"),TeX("$2^{14}$"))
+
+################################################################################
+# PATH TO LOAD THE DATA
+################################################################################
+
+path = "~/Documents/2. UNIGE/2023-1 Master Thesis/fexpsmt/Reproducibility/Main/2. MA/"
+
+################################################################################
+# Running time
+################################################################################
+
+# DATA
+time_own_matrix = readRDS(file = paste0(path,"time_own.RData"))
+time_r_matrix = readRDS(file = paste0(path,"time_R.RData"))
+
+# AXIS NAMES FOR LATEX
+rownames(time_own_matrix) = paste0("$\\mathbf{",ma_coef_vec,"}$")
+colnames(time_own_matrix) = paste0("$\\mathbf{2^{",POWER,"}}$")
+rownames(time_r_matrix) = paste0("$\\mathbf{",ma_coef_vec,"}$")
+colnames(time_r_matrix) = paste0("$\\mathbf{2^{",POWER,"}}$")
+
+comparison_matrix = as.matrix(time_own_matrix<=time_r_matrix)
+
+# LATEX OUTPUT
+time_own_matrix = xtable(time_own_matrix, digits = 5)
+time_r_matrix = xtable(time_r_matrix, digits = 5)
+print(time_own_matrix, include.rownames = TRUE, hline.after = c(-1,0, nrow(time_own_matrix)), sanitize.text.function = function(x) {x},booktabs = TRUE)
+print(time_r_matrix, include.rownames = TRUE, hline.after = c(-1,0, nrow(time_r_matrix)), sanitize.text.function = function(x) {x})
+
+################################################################################
+# MSE COEFFICIENTS for phi
+################################################################################
+
+# DATA
+fit_own_coef_matrix = readRDS(file = paste0(path,"theta_1_own.RData"))
+fit_r_coef_matrix = readRDS(file = paste0(path,"theta_1_r.RData"))
+
+# AXIS NAMES FOR LATEX
+rownames(fit_own_coef_matrix) = paste0("$\\mathbf{",ma_coef_vec,"}$")
+colnames(fit_own_coef_matrix) = paste0("$\\mathbf{2^{",POWER,"}}$")
+rownames(fit_r_coef_matrix) = paste0("$\\mathbf{",ma_coef_vec,"}$")
+colnames(fit_r_coef_matrix) = paste0("$\\mathbf{2^{",POWER,"}}$")
+
+comparison_matrix = as.matrix(fit_own_coef_matrix<=fit_r_coef_matrix)
+
+# LATEX OUTPUT
+fit_own_coef_matrix = xtable(fit_own_coef_matrix, digits = 5)
+fit_r_coef_matrix = xtable(fit_r_coef_matrix, digits = 5)
+print(fit_own_coef_matrix, include.rownames = TRUE, hline.after = c(-1,0, nrow(fit_own_coef_matrix)), sanitize.text.function = function(x) {x},booktabs = TRUE)
+print(fit_r_coef_matrix, include.rownames = TRUE, hline.after = c(-1,0, nrow(fit_r_coef_matrix)), sanitize.text.function = function(x) {x})
+
+################################################################################
+# MSE COEFFICIENTS for lambda=1
+################################################################################
+
+# DATA
+fit_own_exp_matrix = readRDS(file = paste0(path,"lambda_own.RData"))
+fit_r_exp_matrix = readRDS(file = paste0(path,"lambda_r.RData"))
+
+# AXIS NAMES FOR LATEX
+rownames(fit_own_exp_matrix) = paste0("$\\mathbf{",ma_coef_vec,"}$")
+colnames(fit_own_exp_matrix) = paste0("$\\mathbf{2^{",POWER,"}}$")
+rownames(fit_r_exp_matrix) = paste0("$\\mathbf{",ma_coef_vec,"}$")
+colnames(fit_r_exp_matrix) = paste0("$\\mathbf{2^{",POWER,"}}$")
+
+comparison_matrix = as.matrix(fit_own_exp_matrix<=fit_r_exp_matrix)
+
+# LATEX OUTPUT
+fit_own_exp_matrix = xtable(fit_own_exp_matrix, digits = 5)
+fit_r_exp_matrix = xtable(fit_r_exp_matrix, digits = 5)
+print(fit_own_exp_matrix, include.rownames = TRUE, hline.after = c(-1,0, nrow(fit_own_exp_matrix)), sanitize.text.function = function(x) {x},booktabs = TRUE)
+print(fit_r_exp_matrix, include.rownames = TRUE, hline.after = c(-1,0, nrow(fit_r_exp_matrix)), sanitize.text.function = function(x) {x})
+
+################################################################################
+# Correct p-values for EXP(1)
+################################################################################
+
+# DATA
+p_val_own_exp_matrix = readRDS(file = paste0(path,"p.val_own.RData"))
+p_val_r_exp_matrix = readRDS(file = paste0(path,"p.val_R.RData"))
+
+# AXIS NAMES FOR LATEX
+rownames(p_val_own_exp_matrix) = paste0("$\\mathbf{",ma_coef_vec,"}$")
+colnames(p_val_own_exp_matrix) = paste0("$\\mathbf{2^{",POWER,"}}$")
+rownames(p_val_r_exp_matrix) = paste0("$\\mathbf{",ma_coef_vec,"}$")
+colnames(p_val_r_exp_matrix) = paste0("$\\mathbf{2^{",POWER,"}}$")
+
+comparison_matrix = as.matrix(p_val_own_exp_matrix<p_val_r_exp_matrix)
+
+# LATEX OUTPUT
+p_val_own_exp_matrix = xtable(p_val_own_exp_matrix, digits = 5)
+p_val_r_exp_matrix = xtable(p_val_r_exp_matrix, digits = 5)
+print(p_val_own_exp_matrix, include.rownames = TRUE, hline.after = c(-1,0, nrow(p_val_own_exp_matrix)), sanitize.text.function = function(x) {x},booktabs = TRUE)
+print(p_val_r_exp_matrix, include.rownames = TRUE, hline.after = c(-1,0, nrow(p_val_r_exp_matrix)), sanitize.text.function = function(x) {x})
